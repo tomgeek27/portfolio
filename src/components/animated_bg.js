4335e000543261e401bd2f2ee7d22be49012d05d
Tommaso amadori <mmito910@gmail.com>
2021-01-29 01:11:05 +0100

    Factorized particles component

---
+++ src/components/animated_bg.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import Particles from "react-tsparticles";
+
+function AnimatedBg() {
+  return (
+    <Particles
+      style={{
+        position: "fixed",
+        padding: "0",
+        margin: "0",
+
+        top: "0",
+        left: "0",
+
+        width: "100%",
+        height: "100%",
+        zIndex: -10
+      }}
+      id="tsparticles"
+      options={{
+        background: {
+          color: {
+            value: "#000000",
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: false,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  )
+}
+
+export default AnimatedBg;

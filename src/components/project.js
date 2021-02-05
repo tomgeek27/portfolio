a8858a47f0d65b8ffe4be6e3710284aadf6c16a1
Tommaso Amadori <tommi27@live.it>
2021-02-05 12:13:02 +0100

    fix project dimension in small screens

--- src/components/project.js
+++ src/components/project.js
@@ -4,7 +4,7 @@ function Project(props) {
   return (
     <div className="margin17">
       <a href={props.url} className="project flex-column vertical-center horizontal-center">
-        <img src={props.src} style={{ width: '150px', height: '150px' }} />
+        <img className="img-project" src={props.src} alt=""/>
         <div className="flex-column vertical-center margin43h margin23top">
           <span className="title-project">{props.title}</span>
           <div className="subtitle-project margin5top">{props.subtitle}</div>

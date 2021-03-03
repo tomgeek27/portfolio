91b6784bdf339b74fd7e9ab7423062e7c7ce55e6
Tommaso amadori <mmito910@gmail.com>
2021-03-03 01:23:46 +0100

    REFACTORING: replaced skill col  logic with bootstrap col logic

--- src/constants/experiences_elements.js
+++ src/constants/experiences_elements.js
@@ -9,13 +9,13 @@ const educations = [
     date: '2016 - 2019',
     title: 'Bachelor\'s degree in Computer Science',
     location: 'Università statale degli studi di Milano',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
+    description: 'My study at University of Milan was mainly computer science\'s basis based. In particular i loved studying operating system and software engineering. During this period I also learned different programming language at University and on my own. At the end of the studies, as a thesis, I studied face recognition in small images using an algorithm based on fuzzy sets'
   },
   {
     date: '2011 - 2016',
     title: 'Diploma of maturity as computer expert',
     location: 'ITIS G. Feltrinelli',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
+    description: 'I graduated highschool, where I enojoyed doing scientific school subjects. In particular i liked math and computer science. During that period I developed my first Unity game project.'
   },
 ]
 
@@ -24,7 +24,7 @@ const career = [
     date: '2015',
     title: 'Stage Beta80',
     location: 'Beta80 Group',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
+    description: 'I worked as intern and i learned how to work in team and i focused on develop libraries for internal use'
   },
 ]
 

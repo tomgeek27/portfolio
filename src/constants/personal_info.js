c4c4b12cf91ec2c3ccefdb613c2a25232a321c8b
Tommaso Amadori <tommaso.amadori.dev@gmail.com>
2021-11-08 21:47:35 +0100

    updated my info

--- src/constants/personal_info.js
+++ src/constants/personal_info.js
@@ -15,8 +15,8 @@ const age = () => {
 }
 
 const aboutme = {
-  it: "Ciao! Sono uno studente e freelance front-end developer. Sono un ragazzo sempre pronto a sperimentare e imparare nuovi strumenti e nuove tencologie da poter utilizzare. Ho iniziato a programmare da quando sono adolescente facendo semplici programmini, continuando poi nel mondo dei videogiochi sviluppandone diversi fino ad arrivare allo sviluppo di applicazioni lato front-end.",
-  en: "Hi, I am a student and freelance front-end developer and I’m passionate about experimenting and learning new technologies. I’m into programming since i was a child. I began by developing small and simple programs, then, moved by the passion for video games, I started developing different ones up to the front-end development which I'm currently specializing on"}
+  it: "Ciao! Sono uno studente e freelance full-stack developer. Sono un ragazzo sempre pronto a sperimentare e imparare nuovi strumenti e nuove tencologie da poter utilizzare. Ho iniziato a programmare da quando sono adolescente facendo semplici programmi, continuando poi nel mondo dei videogiochi sviluppandone diversi fino ad arrivare allo sviluppo di applicazioni lato front-end e back-end.",
+  en: "Hi, I am a student and freelance fulls-tack developer and I’m passionate about experimenting and learning new technologies. I’m into programming since i was a child. I began by developing small and simple programs, then, moved by the passion for video games, I started developing different ones up to the front-end and back-end development which I'm currently specializing on"}
 
 
 export {email, location, age, aboutme}

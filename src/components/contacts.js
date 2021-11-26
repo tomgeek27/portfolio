43b8f5a017e56b8d24a788d38e74810af0047c71
tomgeek27 <tommaso.amadori.dev@gmail.com>
2021-11-26 22:40:27 +0100

    added copyrights

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -12,6 +12,16 @@ const CONTACTS = {
   en: 'Contacts'
 }
 
+const COPYRIGHT_TEXT = {
+  it: 'portfolio di Tommaso Amadori. Tutti i diritti riservati',
+  en: 'Tommaso Amadori\'s portofolio. All Rights Reserved'
+}
+
+const copyright = () => {
+  let currentYear = new Date().getFullYear()
+  return `\xA9 Copyright ${currentYear}, ${getTranslated(COPYRIGHT_TEXT)}`
+}
+
 function Contacts() {
   return (
     <div style={{width: '100%', backgroundColor: '#343434'}}>
@@ -31,6 +41,7 @@ function Contacts() {
             <SocialButton src={maillogo} url={mail_url} />
           </div>
       </div>
+      <div id="copyright">{copyright()}</div> 
     </div>
   );
 }

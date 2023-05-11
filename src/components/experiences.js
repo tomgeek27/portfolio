b2a78d2ee3757d9bab7f44dec5b49b747dcaaf6c
Tommaso Amadori <tommaso.amadori.dev@gmail.com>
2023-05-12 01:03:27 +0200

    revisited icon of timeline

--- src/components/experiences.js
+++ src/components/experiences.js
@@ -18,10 +18,21 @@ const CAREER = {
 function getTimelineEventElement(list) {
 
   const displayElement = (x) => {
+    let iconDim = x.iconDim ?? 40
+    let border = x.iconBorder ?? 0
+    let iconUrl = x.iconUrl ?? 'https://cdn-icons-png.flaticon.com/128/4457/4457646.png'
     return (
       <VerticalTimelineElement
         className="custom-icon custom-container"
         date={getTranslated(x.date)}
+        iconStyle={{
+          backgroundImage: `url(${iconUrl})`,//'url(https://cdn-icons-png.flaticon.com/128/3313/3313480.png)',
+          backgroundSize: `${iconDim}px ${iconDim}px`,
+          backgroundPosition: '50% 50%',
+          backgroundRepeat: 'no-repeat',
+          boxShadow: `inset 0px 0px 0px ${border}px #323232, 0px 0px 7px 4px white`
+        }}
+        iconClassName="vertical-timeline-element-icon"
       >
         <div className="exp-title-element">{getTranslated(x.title)}</div>
         <div className="flex-row" style={{marginTop: '5px'}}>
@@ -49,15 +60,17 @@ function Experiences(props) {
     }} cit={"All things would be better if they could be done twice."} author={"Johann W. Goethe"} childrenClasses={"no-jump-animated"}>
       <div className="width-sized-margined">
         <div className="exp-title">
-          {getTranslated(EXPERIENCES)}
-        </div>
-        <div style={{ marginBottom: '80px' }}>
-          {getTimelineEventElement(props.educations)}
-        </div>
-        <div className="exp-title">
           {getTranslated(CAREER)}
         </div>
-        {getTimelineEventElement(props.career)}
+        <div style={{ marginBottom: '80px' }}>
+          {getTimelineEventElement(props.career)}
+        </div>
+        <div className="exp-title">
+          {getTranslated(EXPERIENCES)}
+        </div>
+        <div>
+          {getTimelineEventElement(props.educations)}
+        </div>
         {/* <div style={{ minHeight: '150px' }} /> */}
       </div>
     </Content>
--- src/constants/experiences_elements.js
+++ src/constants/experiences_elements.js
@@ -5,6 +5,8 @@ const educations = [
       it: 'Laurea Magistrale in Informatica',
       en: 'Master\'s degree in Computer Science'
     },
+    iconUrl: `https://upload.wikimedia.org/wikipedia/it/thumb/0/04/Logo_Universit%C3%A0_degli_Studi_di_Milano.svg/1200px-Logo_Universit%C3%A0_degli_Studi_di_Milano.svg.png`,
+    iconDim: 35,
     location: 'Università statale degli studi di Milano',
     description: {
       it: 'Gli ultimi due anni di studio si sono concentrati su uno sviluppo pratico di diversi progetti basati su diversi framework e linguaggi di programmazione. Si è conclusa con un tirocinio interno di oltre 6 mesi sullo studio, ricerca e sviluppo di un framework scritto in python per la stima della pressione sanguigna a partire da un semplice video-selfie.',
@@ -17,6 +19,8 @@ const educations = [
       it: 'Laurea Triennale in Informatica',
       en: 'Bachelor\'s degree in Computer Science'
     },
+    iconUrl: `https://upload.wikimedia.org/wikipedia/it/thumb/0/04/Logo_Universit%C3%A0_degli_Studi_di_Milano.svg/1200px-Logo_Universit%C3%A0_degli_Studi_di_Milano.svg.png`,
+    iconDim: 35,
     location: 'Università statale degli studi di Milano',
     description: {
       it: 'I miei studi all\'Università si sono basati principalmente sulle basi dell\'informatica. Lo studio dei sistemi operativi e dell\'ingegneria del software sono stati la parte a mio parere maggiormente interessanti. Ho avuto modo di imparare diversi linguaggi di programmazione, grazie ai corsi universitari e alla mia passione per questo studio. Il mio progetto di tesi riguardava il riconoscimento facciale in piccole immagini utilizzando un algoritmo basato su fuzzyset.',
@@ -28,6 +32,9 @@ const educations = [
       it: 'Diploma di maturità come perito informatico',
       en: 'High school diploma as computer expert'
     },
+    iconUrl: `https://cdn-icons-png.flaticon.com/128/3313/3313480.png`,
+    iconDim: 25,
+    iconBorder: 2,
     location: 'ITIS G. Feltrinelli',
     description: {
       it: 'Durante il periodo delle scuole superiori ho apprezzato particolarmente le materie scientifiche come matematica e informatica. Sempre in quel periodo ho anche sviluppato il mio primo videogioco con Unity.',
@@ -37,9 +44,36 @@ const educations = [
 
 const career = [
   {
+    date: {
+      it: 'Febbraio 2023 - Presente',
+      en: 'February 2023 - Present'
+    },
+    title: 'Software Engineer',
+    location: 'Intesa Sanpaolo S.p.A.',
+    iconUrl: `https://companieslogo.com/img/orig/ISP.MI-e8dc3cc4.png?t=1593230015`,
+    iconDim: 28,
+    description: {
+      it: 'Esperienza da sviluppatore in ambito bancario nell\'implentanzione di microservizi legati all\'ambito dei canali di comunicazione. Al passo con le tecnologie moderne Java, Spring-boot, Kafka ed altre',
+      en: 'Experience as a developer in the banking sector in the implementation of microservices related to communication channels. Keep up with modern technologies Java, Spring-boot, Kafka and others'}
+  },
+  {
+    date: {
+      it: 'Marzo 2018 - Ottobre 2020',
+      en: 'March 2018 - October 2020'
+    },
+    title: 'Freelancer',
+    location: 'Milano',
+    iconDim: 25,
+    iconBorder: 2,
+    description: {
+      it: 'Ho fatto esperienza da freelancer rapportandomi con clienti in ambito medico con cui ho progettato e sviluppato un\'applicazione del mondo healt-care: Ritm-U',
+      en: 'I gained work experience as freelancer with client in healt-care world with whom I designed and developed a cross-platform application named: Ritm-U'}
+  },
+  {
     date: '2015',
-    title: 'Stage Beta80',
+    title: 'C# Developer',
     location: 'Beta80 Group',
+    iconUrl: `https://pbs.twimg.com/profile_images/1321853832083759110/FswbVAqi_400x400.png`,
     description: {
       it: 'Ho acquisito esperienza lavorativa e capacità di lavorare in team lavorando come stagista. Ho avuto la possiblitá  di sviluppare librerie per un uso interno.',
       en: 'I gained work experience and team-work skills working as an intern, where I deepened my knowledge on developing libraries for internal use.'}
